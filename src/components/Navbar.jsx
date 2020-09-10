7a270da24a1888d406e3168b9a6f7741e5823d77
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-09-10 03:25:21 -0400

    Jonathan Portillo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
+import useDarkMode from "../Hooks/useDarkMode";
 
-const Navbar = (props) => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = (e) => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+const Navbar = () => {
+  const [darkMode, setDarkMode, toggleMode] = useDarkMode(false);
+
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
